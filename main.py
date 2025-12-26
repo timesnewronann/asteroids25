ebf4a5305812fa5aaa3a5869527e8737684d85bd
timesnewronann <ronanborja26@gmail.com>
2025-12-25 22:37:39 -0800

    Added clock

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # create a new clock object
+    clock = pygame.time.Clock()
+
+    # delta time variable set to 0
+    dt = 0
+
     while True:
         log_state()
 
@@ -21,6 +27,12 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # call the clock.tick method
+        clock.tick(60)
+
+        # amount of time that has passed since it was last called
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
