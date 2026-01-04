0c9937c4281995df0775c95b722398bcad738c65
timesnewronann <ronanborja26@gmail.com>
2026-01-03 22:16:14 -0800

    Added asteorids and asteroid field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
 
 
@@ -17,12 +19,18 @@ def main():
 
     # Create groups
     updatable = pygame.sprite.Group()
-    drawable = pygame.sprint.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # before player object instance is created
     Player.containers = (updatable, drawable)
-
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT/2)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
+
+    asteroidField = AsteroidField()
 
     # delta time variable set to 0
     dt = 0
@@ -36,12 +44,19 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        # update the group not the player
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+        # Replace with groups
+        # player.draw(screen)
+        # player.update(dt)
         pygame.display.flip()
 
         # call the clock.tick method
-        clock.tick(60)
+        # clock.tick(60)
 
         # amount of time that has passed since it was last called
         dt = clock.tick(60) / 1000
